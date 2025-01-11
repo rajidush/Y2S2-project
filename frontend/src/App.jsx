4a650cf50b75fbaaa25909d0e7b48e701b83c0f5
Sandarukaushan999 <sandarukaushan999@gmail.com>
2025-01-11 13:27:34 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Solar_recycle from './Solar_recycle'
+import Solar_recycle from './components/solar_recycle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      </div>
-    </>
+    <div>
+       <Solar_recycle></Solar_recycle> 
+    </div>
   )
 }
 
